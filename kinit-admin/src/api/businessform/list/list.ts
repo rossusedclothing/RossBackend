f37364cc52c80df1fd394803ed3b216c8a665dd4
jinlg8288 <you@example.com>
2025-10-16 13:56:47 +0800

    'url路由修正'

--- kinit-admin/src/api/businessform/list/list.ts
+++ kinit-admin/src/api/businessform/list/list.ts
@@ -2,9 +2,15 @@ import request from '@/config/axios'
 
 //表单列表
 export const getBusinessFormLists = (params?: any) => {
-  return request.get({ url: '/businessform/list/index', params })
+  return request.get({ url: '/businessform/bform/list', params })
 }
 
 export const deleteRecord = (ids: number[]) => {
-  return request.delete({ url: '/businessform/list/delete', data: ids })
+  return request.delete({ url: '/businessform/bform/delete', data: ids })
 }
+
+/*
+export const createBusinessForm = (data: any) => {
+  return request.post({ url: '/businessform/bform/create', data })
+}
+*/
