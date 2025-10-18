2cb127eed835eedcf8e24a0e0985e6501cb9c61b
jinlg8288 <you@example.com>
2025-10-18 22:22:00 +0800

    'add-dist'

---
+++ kinit-admin/dist-pro/assets/CenterBottom-B7O8029w.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CenterBottom.vue_vue_type_style_index_0_lang-CN_DxAdG.js";import"./datav-vue3.es-DWw5TDnn.js";import"./vue-chunks-o2h5QaLn.js";export{o as default};
---
+++ kinit-admin/dist-pro/assets/dict-DJyIMlYA.js
@@ -0,0 +1,1 @@
+const n=(s,e)=>{var t;if(e){const r=(t=s.find(l=>l.value===e))==null?void 0:t.label;return r===void 0?"获取失败":r}else return""};export{n as s};
