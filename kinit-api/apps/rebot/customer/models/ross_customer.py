c165aa7903b5efdc4eace784e98839c2d533012f
luojiaju <1918652173@qq.com>
2025-10-25 23:55:12 +0800

    feat(customer): 扩展客户模型字段
    - 增加用户标签、摘要内容字段
    - 添加销售代表和业务员手机号字段
    - 新增是否打标签状态字段
    - 添加问题进度字段- 扩展客户手机号和名称字段长度至125字符

--- kinit-api/apps/rebot/customer/models/ross_customer.py
+++ kinit-api/apps/rebot/customer/models/ross_customer.py
@@ -29,11 +29,16 @@ class ReBotCustomer(BaseModel):
     __table_args__ = ({'comment': '客户表'})
 
     sales_agent_id = mapped_column(String(20), default=1, nullable=True, comment='所属业务员')
-    customer_phone = mapped_column(String(20), nullable=True, comment='客户手机号', index=True,unique= True)
-    customer_name = mapped_column(String(50), nullable=True, comment='客户名称', index=True)
+    customer_phone = mapped_column(String(125), nullable=True, comment='客户手机号', index=True,unique= True)
+    customer_name = mapped_column(String(125), nullable=True, comment='客户名称', index=True)
     platform = mapped_column(String(20), default='whatsapp', nullable=True, comment='平台')
     meta_data = mapped_column(Text, nullable=True, comment='元数据')
-
+    user_tag = mapped_column(String(50), nullable=True, comment='用户标签')
+    summary_content = mapped_column(Text, nullable=True, comment='摘要内容')
+    sales_repp_phone = mapped_column(String(20), nullable=True, comment='销售代表手机号')
+    sales_agent_phone = mapped_column(String(20), nullable=True, comment='业务员手机号')
+    is_tagged = mapped_column(Integer, default=0, nullable=True, comment='是否打标签 0 未打标签 1 已打标签')
+    answering_progress = mapped_column(Integer, default=1, nullable=True, comment='问题进度')
 
 class ReBotCustomerMessage(BaseModel):
     __tablename__ = "ross_customer_history"
